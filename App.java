4ce0d78ef31f661a4c2eb05fc3933e7555530994
Nagy <janos@zold.lan>
2022-02-16 09:58:30 +0100

    'Kezdes'

---
+++ App.java
@@ -0,0 +1,21 @@
+/*
+* File: App.java
+* Author: Nagy János
+* Copyright: 2022, Nagy János
+* Group: Szoft I N
+* Date: 2022-02-16
+* Github: https://github.com/andteki/
+* Licenc: GNU GPL
+*/
+
+
+public class App {
+	
+	public static void main (String[] args) {
+		System.out.println("Nagy Janos, 2022-02-16, Szoft I N");
+		System.out.println("Feladat 0306");
+		System.out.println("Haromszog teruletszamitas");
+		
+		
+	}
+}
